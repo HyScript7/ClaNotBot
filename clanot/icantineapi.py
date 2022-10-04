e2349b2b6ee737afa04f7103345aab82f3944418
mobilex1122 <60009551+mobilex1122@users.noreply.github.com>
2022-10-04 14:20:32 +0200

    Přidáno GetToday

--- clanot/icantineapi.py
+++ clanot/icantineapi.py
@@ -1,8 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+import re
 
 
 
 def GetToday(URL:str):
-    pass
+
+    req= requests.get('http://jidelna.gasos-ro.cz')
+
+    soup = BeautifulSoup(req.text, "html.parser")
+
+    first = soup.find("div",attrs={"class":"jidelnicekDen"}).find_all("div", attrs={"class":"column jidelnicekItem"})
+
+    return {"1": first[0].get_text().strip(), "2": first[1].get_text().strip()}
 
 def GetNext(URL:str):
     pass
+
+
+
+print(GetToday("https://jidelna.gasos-ro.cz/"))
